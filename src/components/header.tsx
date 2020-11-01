bd2c00a708351a96c97f6047ef3492f5cee6d174
ohsugi5532 <t.ohsugi5532@gmail.com>
2020-11-01 22:19:48 +0900

    Refactor

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Header: React.FC = () => {
+  return (
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <title>johnny.blog</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+};
+
+export default Header;
